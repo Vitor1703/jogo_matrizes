dadc95b84248f9749b311b84825cb9d1962ff6ea
Vitor Hugo <vitorhc58@gmail.com>
2022-11-23 17:15:21 -0300

    ajustes jogo matrizes

--- jogo_matrizes.py
+++ jogo_matrizes.py
@@ -1,32 +1,36 @@
 import random
 
 class Matriz:
-    def __init__(self, col, row, name):
-        self.coluna = col
+    def _init_(self, row, col, name):
         self.linha = row
+        self.coluna = col
         self.nome = name
 
     def matrizBot(self):
         matriz = []
         linha = []
 
-        while len(matriz) != self.linha: # Quando o número de elementos da matriz(linhas) forem diferentes da quantidade máxima definida pelo usuário, ele ficará rodando.
-            n = random.randint(0,99) # Utilizei random para adicionar os valores
-            linha.append(n) # Adiciono n à linha
+        while len(matriz) != self.linha:
+            n = random.randint(0,99)
+            linha.append
 
-            if len(linha) == self.coluna: # Se a quantidade de elementos for igual à quantidade de colunas definida pelo usuário :
-                matriz.append(linha) # Adiciono a linha à matriz
+            if len(linha) == self.coluna:
+                matriz.append(linha)
                 linha = []
 
         return matriz
 
-    def matrizUsuario(self):
+    def matrizJogador(self):
         matriz = []
         linha = []
 
+        print()
+        print(f'OK {self.nome}, agora vamos adicionar os valores em sua matriz {self.linha}/{self.coluna}')
+        print()
+
         while len(matriz) != self.linha:
-            n = int(input("Digite um número: "))
-            linha.append(n)
+            n = int(input('Digite o valor(De 0 a 99): '))
+            linha.append
 
             if(len(linha) == self.coluna):
                 matriz.append(linha)
@@ -39,14 +43,10 @@ class Matriz:
         pPes = 0
 
         for l in range(self.linha):
-            line = []
-
             for c in range(self.coluna):
                 if m1[l][c] > m2[l][c]:
-                    line.append(m1[l][c])
                     pBot += 1
                 else:
-                    line.append(m2[l][c])
                     pPes += 1
 
         if pPes > pBot:
@@ -58,16 +58,16 @@ class Matriz:
 
         print(f'\nPontuação {self.nome}: [{pPes}]\nPontuação BOT: [{pBot}]\n')
 
+nomeJogador = str(input('Olá jogador, digite seu nome --> '))
+linhas = int(input('Digite a quantidade de linhas da sua MATRIZ --> '))
+colunas = int(input('Digite a quantidade de colunas da sua MATRIZ --> '))
 
-nomeUsuario = str(input('Olá jogador, digite seu nome: '))
-
-
-matriz = Matriz(2, 2, nomeUsuario)
-m1 = matriz.matrizBot()
-m2 = matriz.matrizUsuario()
+matriz = Matriz(linhas, colunas, nomeJogador)
+matrizBot = matriz.matrizBot()
+matrizJogador = matriz.matrizJogador()
 
 print()
-print('Bot: ', m1)
-print('Usuário: ', m2)
+print(f'Matriz BOT --> {matrizBot}')
+print(f'Matriz {nomeJogador} --> {matrizJogador}')
 
-compara = matriz.comparaMatriz(m1, m2)
+matriz.comparaMatriz(matrizBot, matrizJogador)
